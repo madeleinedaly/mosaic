7e7c157ced265d2bc76f20624577998962d9af5a
Madeleine Daly <mdaly@dailybreak.com>
2017-12-30 18:17:58 -0500

    pad filenames

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ index.load_video(sys.argv[1], offset=int(sys.argv[3]))
 index.init_indexes()
 for i, frame in enumerate(index.generate_video(480, 640, 240)):
     print i
-    Image.fromarray(frame).save('%s/%d.png' % (sys.argv[2], i))
+    Image.fromarray(frame).save('%s/%03d.png' % (sys.argv[2], i))
